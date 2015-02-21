a0eb051c85b29d5568ef5d3bae3583a43caa01ba
kelseyljacobsen <kelseyljacobsen@gmail.com>
2015-02-21 14:43:07 -0800

    more red dots

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+  $('#knee, #knee2, #knee3, #knee4').click(function() {
+    $('*').removeClass('clicked-body-part');
+    $(this).addClass('clicked-body-part');
+    $('#body-part').html('Knee');
+  });
+
+  $('#ankle, #ankle2, #ankle3, #ankle4').click(function() {
+    $('*').removeClass('clicked-body-part');
+    $(this).addClass('clicked-body-part');
+    $('#body-part').html('Ankle');
+  });
+
+  $('#foot, #foot2').click(function() {
+    $('*').removeClass('clicked-body-part');
+    $(this).addClass('clicked-body-part');
+    $('#body-part').html('Foot');
+  });
+
+  $('#hand, #hand2, #hand3, #hand4').click(function() {
+    $('*').removeClass('clicked-body-part');
+    $(this).addClass('clicked-body-part');
+    $('#body-part').html('Hand');
+  });
+
+  $('#elbow, #elbow2, #elbow3, #elbow4').click(function() {
+    $('*').removeClass('clicked-body-part');
+    $(this).addClass('clicked-body-part');
+    $('#body-part').html('Elbow');
+  });
+
+  $('#wrist, #wrist2, #wrist3, #wrist4').click(function() {
+    $('*').removeClass('clicked-body-part');
+    $(this).addClass('clicked-body-part');
+    $('#body-part').html('Wrist');
+  });
+
+  $('#shoulder, #shoulder2, #shoulder3, #shoulder4').click(function() {
+    $('*').removeClass('clicked-body-part');
+    $(this).addClass('clicked-body-part');
+    $('#body-part').html('Shoulder');
+  });
+
+  $('#spine, #spine2, #spine3, #spine4, #spine5').click(function() {
+    $('*').removeClass('clicked-body-part');
+    $(this).addClass('clicked-body-part');
+    $('#body-part').html('Back/Spine');
+  });
+
+  $('#hip, #hip2, #hip3, #hip4').click(function() {
+    $('*').removeClass('clicked-body-part');
+    $(this).addClass('clicked-body-part');
+    $('#body-part').html('Hip');
+  });
+
+});
